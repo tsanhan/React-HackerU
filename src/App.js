a88662c5383a38f93c75744c2b38fcb802749903
Victor <tsanhan@gmail.com>
2021-05-09 08:12:28 +0300

    chapter7

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
+
 import React from "react";
 import "./App.css";
-
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
-
 import Home from "./components/home";
 import About from "./components/about";
 import Signup from "./components/signup";
+import Signin from "./components/signin";
 
 import { Switch, Route } from "react-router-dom";
-
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
     return (
         <React.Fragment>
+            <ToastContainer />
             <header>
                 <Navbar />
             </header>
-            <main>
+            <main style={{ minHeight: 900 }}>
                 <Switch>
+                    <Route path="/signin" component={Signin} />
                     <Route path="/signup" component={Signup} />
                     <Route path="/about" component={About} />
                     <Route path="/" exact component={Home} />
@@ -37,7 +40,7 @@ function App() {
                 <Footer />
             </footer>
         </React.Fragment>
-    )
+    );
 }
 
 export default App;
